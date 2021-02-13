e588fb9df66a9429bb7491fe20cb34c062be1ba0
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-02-13 11:42:55 -0500

    Follow code tutorial for bootstrap sidebar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <Container fluid>
+            <Row>
+                <Col xs={2} id="sidebar-wrapper">      
+                  <Sidebar />
+                </Col>
+                <Col  xs={10} id="page-content-wrapper">
+                    this is a test
+                </Col> 
+            </Row>
+
+        </Container>
+    );
 }
 
 export default App;
