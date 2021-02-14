3d9f704259286d121da788d93ec373911e417d09
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-02-14 17:29:15 -0500

    Start dropdown subnav

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Sidebar = props => {
-
+    const [isDropdownCollapsed, setIsDropdownCollapsed] = useState(true);
+    const handleDropdownCollapse = (e) => {
+        e.preventDefault();
+        setIsDropdownCollapsed(!isDropdownCollapsed);
+    }
     return (
         <>
             <h4 className="title text-center font-weight-bold"><Link to="/">kelvin<span>_</span>kellner<span>.</span></Link></h4>
             <ul class="menu">
                 <li><NavLink exact to="/" activeClassName="active">about me.</NavLink></li>
-                <li><NavLink exact to="/projects" activeClassName="active">projects.</NavLink></li>
+                <li><Link onClick={ e => handleDropdownCollapse(e) }>projects.</Link></li>
+                <ul class={`menu dropdown${isDropdownCollapsed ? ' collapse' : ''}`}>
+                    <li>ux design.</li>
+                    <li>software development.</li>
+                    <li>graphic design.</li>
+                </ul>
                 <li><NavLink to="/hobbies" activeClassName="active">hobbies.</NavLink></li>
                 <li><NavLink to="/contact" activeClassName="active">contact.</NavLink></li>
             </ul>
