acc38ea14f93aa757cbd0cc5d0329e5481e055a6
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-02-18 18:22:54 -0500

    Progress on making ContactForm functional

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 
 const ContactForm = () => {
     const [validated, setValidated] = useState(false);
-    
+    const [status, setStatus] = useState(0);
 
     const handleSubmit = e => {
         const form = e.currentTarget;
+        setStatus(0);
         if(form.checkValidity() === false) {
             e.preventDefault();
             e.stopPropagation();
@@ -29,25 +31,49 @@ const ContactForm = () => {
                 subject: data.subject,
                 message: data.message
             };
-            await emailjs.send(
-                process.env.REACT_APP_SERVICE_ID,
-                process.env.REACT_APP_TEMPLATE_ID,
-                templateParams,
-                process.env.REACT_APP_USER_ID
-            ).then(function(response) {
-                console.log('SUCCESS!', response.status, response.text);
-                //setValidated(false);
-                //form.reset();
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
+            console.log(data);
+            console.log(form);
+            console.log(templateParams);
+            // await emailjs.send(
+            //     process.env.REACT_APP_SERVICE_ID,
+            //     process.env.REACT_APP_TEMPLATE_ID,
+            //     templateParams,
+            //     process.env.REACT_APP_USER_ID
+            // ).then(function(response) {
+            //     console.log('SUCCESS!', response.status, response.text);
+            //     setValidated(false);
+            //     form.reset();
+            //     setStatus(1);
+            // }, function(error) {
+            //     console.log('FAILED...', error);
+            //     setStatus(-1);
+            // });
         } catch (e) {
             console.log(e);
         }
     };
 
+    const alert = () => {
+        if(status===1) {
+            return (
+                <Alert variant="success" style={{padding: "2rem", marginBottom:"2rem"}} onClose={() => setStatus(0)} dismissible>
+                    <Alert.Heading className="font-weight-bold">Your message was sent successfully!</Alert.Heading>
+                    <p style={{marginBottom: 0}}>Thank you for taking the time to contact me, I will get back to you as soon as I can.</p>
+                </Alert>)
+        } else if(status===-1) {
+            return (
+                <Alert variant="danger" onClose={() => setStatus(0)} dismissible>
+                    <Alert.Heading>Your message failed to send!</Alert.Heading>
+                    <p>Feel free to contact me on social media, and I will fix this issue as soon as I can. Sorry about that!</p>
+                </Alert>)
+        } else return;
+    }
+
     return (
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
+
+            {alert()}
+
             <Form.Row>
                 <Form.Group xs={12} lg={6} as={Col} controlId="formContactName">
                 <Form.Label>Name</Form.Label>
